ac08c66b8cbfdeaaf18900eb359ae810c0d9b41f
Jadilson Junior <jadilson.pessanha@i9xp.com.br>
2021-02-12 22:42:53 -0300

    Cards página inicial alterados com novas metricas

--- view/index.php
+++ view/index.php
@@ -9,25 +9,24 @@
 
   $account_id = buscarClientId($id_user); # Busca o Google Account ID
   $permissao  = verificarPermissao($id_user); 
-  $dfatos     = dadosParciaisFato($permissao,$id_user); # Retorna os valores da campanha do usuário logado
 
  ?>
 
 <style type="text/css">
   #cartao-red {
-    background-color: #DB2828;
+    background-color: #DA542E;
     color: #fff;
   }
-  #cartao-blue {
-    background-color: #2185D0;
+  #cartao-lblue {
+    background-color: #27A9E3;
     color: #fff;
   }
   #cartao-green {
-    background-color: #21BA45;
+    background-color: #28B779;
     color: #fff;
   }
-  #cartao-yellow {
-    background-color: #FBBD08;
+  #cartao-blue {
+    background-color: #2255A4;
     color: #fff;
   }
 
@@ -39,7 +38,7 @@
     font-size: 26pt;
   }
 
-  #cadLead, #atribuir-leads,  {
+  #cadLead, #atribuir-leads  {
     display: none;
   }
 
@@ -84,86 +83,27 @@
   <!-- <div id="manipular"></div> -->
 <h1 class="ui center aligned header"><i class="ui tachometer alternate icon"></i>Painel</h1>
 <div class="ui divider"></div>
-<div class="ui grid">
-  <div class="seven wide column">
-    <h3 class="ui left aligned left floated header">
-      Campanha Google
-      <!-- <div class="sub header">Informações gerais sobre a sua campanha no Google AdWords.</div> -->
-      <div class="sub header">Informações gerais sobre a sua campanha no Google AdWords.</div>
-    </h3>
-  </div>
-  <div class="seven wide right floated column">
-  </div>
-</div>
-<br>
-<div id="manipular">
-  <div class="ui four cards">
-    <div id="cartao-blue" class="fluid card">
-      <div class="content">
-        <div id="cartao-blue" class="header">
-          <p>Clicks
-          <i class="right floated hand point right inverted large icon"></i></p>
-          <p id="valor-php" class="ui center aligned">
-            <?php echo number_format($dfatos['cliques'],0,".","."); ?>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div id="cartao-yellow" class="fluid card">
-      <div class="content">
-        <div id="cartao-yellow" class="header">
-          <p>Custo
-          <i class="right floated credit card outline inverted large icon"></i></p>
-          <p id="valor-php2" class="ui center aligned">
-            <?php echo "R$ " . number_format($dfatos['custo'],2,",","."); ?>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div id="cartao-red" class="fluid card">
-      <div class="content">
-        <div id="cartao-red" class="header">
-          <p>Conversões
-          <i class="right floated sync alternate large inverted icon"></i></p>
-          <p id="valor-php" class="ui center aligned">
-            <?php //echo number_format($dfatos['conversoes'],0,".","."); ?>
-            <?php 
-              $conversoes = getConversoes($unidade_id); 
-              echo $conversoes;
-            ?>
-          </p>
-        </div>
-      </div>
-    </div>
-    <div id="cartao-green" class="fluid card">
-      <div class="content">
-        <div id="cartao-green" class="header">
-          <p>Custo por Conversão
-          <i class="right floated credit card inverted large icon"></i></p>
-          <p id="valor-php2" class="ui center aligned">
-            <?php 
-              if ($conversoes == 0) {
-                $cpl = 0;
-              } else {
-                $cpl = $dfatos['custo']/$conversoes;
-              }
-              echo "R$ " . number_format($cpl,2,",","."); ?>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+
+<?php 
+
+  if ($permissao >= 4) {
+    $dfatos = dadosParciaisFato($permissao,$unidade_id); 
+    include 'cards_associado.php';
+  } else {
+    $dadosCards = dadosParciaisFatoAdm();
+    include 'cards_adm.php';
+  }
+
+?>
+
 <br>
 <div class="ui divider"></div>
 <div class="ui grid">
   <div class="left aligned column">
     <h3 class="ui header">
-      Visão Geral do Lead
-      <div class="sub header">Relatório de informações dos leads gerados no portal.</div>
+      Gráficos
+      <div class="sub header">Visão detalhada sobre os leads com gráficos diversos.</div>
     </h3>
-
-<?php //include 'tb_leads.php'; # Arquivo separado com a tabela de Leads. ?>
 
 </div>
 </div> 
